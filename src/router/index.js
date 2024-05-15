35edfa868669789396d44a19ca611e0b58e86a20
SungWoo0315 <noel7752@gmail.com>
2024-05-15 12:29:25 +0900

    vue 라우터 동적 적용 완료.
    defineAsyncComponent 를 사용하지 않고
    import.meta.glob() 만으로 동적 import 로 컴포넌트 라우터 적용.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// >>>>>> 수동으로 import
 import HomeView from '../views/HomeView.vue'
-
 // 동적으로 import 하기위한 defineAsyncComponent
-import { defineAsyncComponent } from 'vue'
+// import { defineAsyncComponent } from 'vue'
 
 // 원하는 파일들을 router로 지정하기 위한 배열
 const routes = []
 
-// 디렉토리 내의 Vue 파일을 동적으로 가져와 라우터에 등록
+// 디렉토리 내의 Vue 파일을 동적으로 가져와 라우터에 등록( vite 문법 )
 const files = import.meta.glob('../views/*.vue')
 
 // for문 돌면서 View.vue 에 해당하는 파일 조건으로 name 을 가공한다.
@@ -20,11 +18,11 @@ for (const path in files) {
       .pop()
       .replace(/View\.vue$/, '')
       .toLowerCase()
-    const component = defineAsyncComponent(() => files[path]())
+    // const component = defineAsyncComponent(() => files[path]())
     routes.push({
       path: `/${name}`,
       name: name,
-      component
+      component: files[path] // [해결] : vue 에서 defineAsyncComponent 를 import 하지 않아도, 이런식으로 path를 잡으면 ()=> import ~~~ 시킬 수 있음.
     })
   }
 }
@@ -60,7 +58,7 @@ const router = createRouter({
 export default router
 
 /**
- * TODO
+ * 2024_05_13
  *
  * 큰 문제는 아니지만, 동적으로 배열로 만들어서 라우터 할 경우
  * [Vue Router warn]: Component "default" in record with path "/about" is defined using "defineAsyncComponent()". Write "() => import('./MyPage.vue')" instead of "defineAsyncComponent(() => import('./MyPage.vue'))".
@@ -70,5 +68,12 @@ export default router
  * 명시적으로 import 해주어야 경고가 안뜬다.
  *
  * 이 문제는, 동적으로 import 해주어야 할 파일이 엄청나게 많지 않는 이상 수동으로 import 해주는게 좋을듯한?
+ *
+ *
+ * 2024_05_14 해결...
+ *
+ * defineAsyncComponent() 를 사용하지 않고도, component 를 동적으로 가져 올 수 있음.
+ * import.meta.glob 에서 [경로] 만 지정해주면 () => import 시켜 주는것을 확인.
+ *
  *
  *  */
